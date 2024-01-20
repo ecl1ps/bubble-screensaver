aa7f0076e16121aacda60eccf90c692f1723fa0f
JeffDChapman <jeffdchapman@hotmail.com>
2024-01-20 11:48:04 -0800

    WIP Free Range
    Allow some bubbles to escape! Also slightly increase size of bouncer box.

--- Bubbles/Elements/UpdatableSphere.cs
+++ Bubbles/Elements/UpdatableSphere.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -12,7 +13,9 @@ namespace Bubbles.Elements
         private Vector direction;
         private readonly double radius;
         private readonly double speed;
-        private bool collidedInLastUpdate;
+        private double BoxMult = 1.25;
+        private bool FreeRange = false;
+        private Random escapeChance = new Random();
 
         public UpdatableSphere(Size bounds, DrawingGroup ellipse, Vector position, Vector direction, double speed, double radius)
         {
@@ -32,13 +35,37 @@ namespace Bubbles.Elements
 
         private void CheckOutOfBounds(Size bounds)
         {
+            if (FreeRange) 
+            { 
+                CheckDistantBounds(bounds);
+                return;
+            }
             var pos = GetPosition();
+            double saveX = direction.X;
+            double saveY = direction.Y;
 
-            if (pos.X - radius <= 0 && direction.X < 0) { direction.X *= -1; }
-            if (pos.X + radius >= bounds.Width && direction.X > 0) { direction.X *= -1; }
+            double bigWidth = bounds.Width * BoxMult;
+            double bigHeight = bounds.Height * BoxMult;
+            double minLeft = 0 - (bigWidth - bounds.Width) / 2;
+            double maxRight = bounds.Width + (bigWidth - bounds.Width) / 2;
+            double minTop = 0 - (bigHeight - bounds.Height) / 2;
+            double maxBottom = bounds.Height + (bigHeight - bounds.Height) / 2;
 
-            if (pos.Y - radius <= 0 && direction.Y < 0) { direction.Y *= -1; }
-            if (pos.Y + radius >= bounds.Height && direction.Y > 0) { direction.Y *= -1; }
+            if (pos.X - radius <= minLeft && direction.X < 0) { direction.X *= -1; }
+            if (pos.X + radius >= maxRight && direction.X > 0) { direction.X *= -1; }
+
+            if (pos.Y - radius <= minTop && direction.Y < 0) { direction.Y *= -1; }
+            if (pos.Y + radius >= maxBottom && direction.Y > 0) { direction.Y *= -1; }
+
+            if (saveX != direction.X || saveY != direction.Y)
+            {
+                if (escapeChance.Next(100) > 95) { FreeRange = true; }
+            }
+        }
+
+        private void CheckDistantBounds(Size bounds)
+        {
+            return;
         }
 
         public void UpdateGeometric()
